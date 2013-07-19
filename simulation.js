ecad236ae7284f6134a115b94667444dbacde05f
Chris <krestofur@gmail.com>
2013-07-18 18:47:16 -0700

    fixes

--- simulation.js
+++ simulation.js
@@ -2,8 +2,7 @@
 
 // simulation.js: functions for simulating models using numeric.js
 
-function Sim($sbmlDoc) 
-{
+function Sim($sbmlDoc) {
     this.sbmlModel = new SbmlParser($sbmlDoc);
     this.data = this.simulate($sbmlDoc);
 }
@@ -11,11 +10,13 @@ function Sim($sbmlDoc)
 Sim.prototype.simulate = function($sbmlDoc) {
     var sbmlModel = new SbmlParser($sbmlDoc);
     var listOfSpecies = sbmlModel.listOfSpecies;
+
     var species = state.$sbmlDoc.find('species');
     // calculate stoichiometry matrix
     var stoichiometryMatrix = sbmlModel.stoichiometry;
     // finding infix
     var listOfReactionInfix = sbmlModel.listOfReactionInfix;
+
     var f = function(t, x) {
         var odeString = '';
         var count = 0;
@@ -33,16 +34,21 @@ Sim.prototype.simulate = function($sbmlDoc) {
             }
         }
         return eval(odeString);
+
     };
+
     var initialConditions = [];
     for (var i = 0; i < listOfSpecies.length; i++) {
         initialConditions.push(parseFloat($(species[i]).attr('initialAmount')));
     }
+
     var sol = numeric.dopri(0, 50, initialConditions, f, 1e-6, 2000);
+
     var time = sol.x;
     var numSol = numeric.transpose(sol.y);
     // creating a simulation solution data structure
     var data = [];
+
     for (i = 0; i < time.length; i++) {
         var iter = {};
         iter.time = time[i];
